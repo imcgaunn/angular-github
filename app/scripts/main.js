0c9353ff325361c0c7baf04da35e520b6279979f
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2014-12-11 10:37:38 -0500

    clear query input when github user name is changed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,60 +14,65 @@ app.constant('apiURL', 'https://api.github.com/users');
 
 //the controller function for the app 'ReposApp'
 app.controller('ReposCtrl', function ($scope, $http, apiURL) {
-  $scope.user = 'bbatsov'; // default the user property of the model
-  $scope.query = '';       // make sure the query is set to nothing
+    $scope.user = 'bbatsov'; // default the user property of the model
+    $scope.query = '';       // make sure the query is set to nothing
 
-  // when user changes, fetch new json from github API
-  $scope.$watch('user', function(nv, ov) {
-    var reqURL = apiURL + '/' + nv + '/repos';
+    // when user changes, fetch new json from github API
+    $scope.$watch('user', function(nv, ov) {
+        var reqURL = apiURL + '/' + nv + '/repos';
 
-    console.log('user is: ' + nv);
-    console.log('api URL: ' + apiURL + '/' + nv + '/repos');
+        console.log('user is: ' + nv);
+        console.log('api URL: ' + apiURL + '/' + nv + '/repos');
 
-    $http.get(reqURL).
-      success(function(data) {
-        $scope.reposData = data;
-      }).
-      error(function(data, status) {
-        // get useful error information if json
-        // can't be retrieved
-        if (status === 404) {
-          $scope.reposData = {
-            'err': "user doesn't exist",
-            'user': nv
-          };
-        }
-      });
-  });
+        $http.get(reqURL).
+            success(function(data) {
+                $scope.reposData = data;
+            }).
+            error(function(data, status) {
+                // get useful error information if json
+                // can't be retrieved
+                if (status === 404) {
+                    $scope.reposData = {
+                        'err': "user doesn't exist",
+                        'user': nv
+                    };
+                }
+            });
+    });
 
-  // only update 'user' property of scope when button is
-  // pressed
-  $scope.updateUser = function() {
-    var userField = document.getElementById('userField');
-    $scope.user = angular.element(userField).val();
-  }
+    // only update 'user' property of scope when button is
+    // pressed. Reset the query field too because it
+    // probably isn't relevant now that the user has changed.
+    $scope.updateUser = function() {
+        var userField = document.getElementById('userField');
+        $scope.user = angular.element(userField).val();
 
-  $scope.valid = function() {
-    // check if there's an error, if there is create a useful
-    // error message and highlight the input field red.
-    if ($scope.reposData['err'] === "user doesn't exist" ) {
-      $scope.errorReason = 'User "' + $scope.reposData['user'] + '"' +
-        ' does not refer to a valid github account';
-      var userField = document.getElementById('userField');
-      angular.element(userField).css('borderColor', 'red');
-      return false;
+        $scope.query = '';
+        var queryinput = document.getElementById('queryinput');
+        angular.element(queryinput).val('');
     }
 
-    return true;
-  }
+    $scope.valid = function() {
+        // check if there's an error, if there is create a useful
+        // error message and highlight the input field red.
+        if ($scope.reposData['err'] === "user doesn't exist" ) {
+            $scope.errorReason = 'User "' + $scope.reposData['user'] + '"' +
+                ' does not refer to a valid github account';
+            var userField = document.getElementById('userField');
+            angular.element(userField).css('borderColor', 'red');
+            return false;
+        }
+
+        return true;
+    }
 
 
-  // helper function to avoid using '!' to invert truth values in angular expressions
-  $scope.invalid = function() { return !($scope.valid()); }
+    // helper function to avoid using '!' to invert truth values in angular expressions
+    $scope.invalid = function() { return !($scope.valid()); }
 
-  // check if JSON has been loaded
-  $scope.dataLoaded = function() {
-    return ($scope.reposData > 0);
-  }
+    // check if JSON has been loaded
+    $scope.dataLoaded = function() {
+        return ($scope.reposData > 0);
+    }
 
 });
